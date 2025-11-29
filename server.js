729327e399c7dd95f85c1213fc38c9726027e938
Aniruddha Das <102868356+aniruddha2002das@users.noreply.github.com>
2025-11-30 00:50:53 +0530

    Enhance server responses to include APP_ID in greetings

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const APP_ID = process.env.APP_ID || 1111;
 
 app.get('/', (req, res) => {
-    res.send(`Hello World from port ${port}`);
+    res.send(`Hello World from port ${port} and app ID is ${APP_ID}`);
 })
 
 app.get('/:path', (req,res) => {
-    res.send(`Hello World from port ${port} and path ${req.params.path}`);
+    res.send(`Hello World from port ${port} and path ${req.params.path} and app ID is ${APP_ID}`);
 })
 
 app.listen(port, () => {
